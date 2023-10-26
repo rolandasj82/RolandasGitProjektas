04f11cf113511e717a075a6cb28a8a4386a74c4a
Rolandas J <rolandas.jonusys@outlook.com>
2023-10-26 15:07:02 +0300

    Nuskaitytas atminties dydis
    Kintamojo vardas txt

--- Diena09_Uzd1.py
+++ Diena09_Uzd1.py
@@ -1,7 +1,10 @@
-txt="Naujas savarankiškas projektas"
+txt = "Naujas savarankiškas projektas"
 print(txt)
 
 for r in txt:
-    print(r,end=" ")
+    print(r, end=" ")
 print()
 print("Nuskaitymas po vieną raide baigtas! \nPabaiga")
+
+# Papildomas kodas
+print(f"Programoje kintamasis 'txt' užima atminties {txt.__sizeof__()} baitų")
